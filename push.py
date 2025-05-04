e52eb62c0a2d86cec54f3dd1943413a6af37eabb
V1K1NGbg <victor@ilchev.com>
2025-05-04 14:15:17 +0200

    fix big push

--- push.py
+++ push.py
@@ -9,5 +9,5 @@ os.system('cd docker-velocity && python3 push.py && cd ..')
 os.system('cd docker-vanilla && python3 push.py && cd ..')
 # os.system('cd docker-waterfall && python3 push.py && cd ..')
 
-os.system('cd Discord-bot && docker build -t v1k1ngbg/discord-bot ./Discord-bot/ && docker push v1k1ngbg/discord-bot && cd ..')
-os.system('docker run -d --restart=always -v ~/server/config.json:/usr/bot/config/config.json -v /var/run/docker.sock:/var/run/docker.sock --name discord-bot v1k1ngbg/discord-bot:latest')
+# os.system('cd Discord-bot && docker build -t v1k1ngbg/discord-bot ./Discord-bot/ && docker push v1k1ngbg/discord-bot && cd ..')
+# os.system('docker run -d --restart=always -v ~/server/config.json:/usr/bot/config/config.json -v /var/run/docker.sock:/var/run/docker.sock --name discord-bot v1k1ngbg/discord-bot:latest')
